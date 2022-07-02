5ee11d23d02aea08763e0c8160559327e6561c9c
TaufiqRizky <taufiq.it2@gmail.com>
2022-07-02 21:16:38 +0700

    fix tala tahap 1-3 & export rootword to excell

--- tfidf.py
+++ tfidf.py
@@ -5,13 +5,23 @@ import math
 import re
     
 def delPrefix(str,prx):
-    if str.startswith(prx):
-        rootWord= str.removeprefix(prx)
-        # print(rootWord)
-        return rootWord
+    prx=prx.split(",")
+    if len(prx) == 1 :
+        if str.startswith(prx[0]):
+            rootWord= str.removeprefix(prx[0])
+            # print(rootWord)
+            return rootWord
+        else:
+            # print("gagal remove prefix")
+            return str
     else:
-        # print("gagal remove prefix")
-        return str
+        if str.startswith(prx[0]):
+            rootWord=prx[1]+str.removeprefix(prx[0])
+            # print(rootWord)
+            return rootWord
+        else:
+            # print("gagal remove prefix")
+            return str
        
 def delSuffix(str,sfx):
     if str.endswith(sfx):
@@ -21,25 +31,14 @@ def delSuffix(str,sfx):
         # print('gagal remove suffix')
         return str
 
-def delParticle(str,ptl):
-    if ptl.endswith(ptl):
-        rootWord= str.removesurffix(ptl)
-        return rootWord
-    else:
-        # print('gagal remove particle')
-        return str
 
-def delPossivePronoun (str, pp):
-    if pp.endswith(pp):
-        rootWord= str.removesurffix(pp)
-        return rootWord
-    else:
-        # print('gagal remove particle')
-        return str
    
 def main():
+    awalan1 =  np.loadtxt('awalan1.txt',dtype="U")
     awalan2 =  np.loadtxt('awalan2.txt',dtype="U")
     akhiran = np.loadtxt('akhiran.txt',dtype="U")
+    kepunyaan = np.loadtxt('kepunyaan.txt',dtype="U")
+    partikel = np.loadtxt('partikel.txt',dtype="U")
     stopWords = np.loadtxt('stopword.txt',dtype="U")
     cStop=len(stopWords)
     # print("Jumlah dokumen: ")
@@ -82,52 +81,55 @@ def main():
             d_index_temp.append(x.count(y))
         d_index.append(d_index_temp)
 
-
+    stem = list()
+    for j in d:
     # tahap 1 algoritma Tala
-    tesString = "persaudaraan"       
-    for i in range(len(partikel)):
-       rootword=delParticle(tesString, partikel[i])
-       if tesString is not rootword:
-        print(rootword)
-        break
-    tesString = rootword
-    # tahap 2 algoritma Tala
-    tesString = "persaudaraan"       
-    for i in range(len(kepunyaan)):
-       rootword=delPossivePronoun(tesString, kepunyaan[i])
-       if tesString is not rootword:
-        print(rootword)
-        break
-    # tahap 3 algoritma Tala
-    tesString = "persaudaraan"       
-    for i in range(len(awalan1)):
-       rootword=delPrefix(tesString, awalan1[i])
-       if tesString is not rootword:
-        print(rootword)
-        break
-    # tahap 4 algoritma Tala 
-    for i in range(len(akhiran)):
-       tesString = "persaudaraan"
-       rootword=delSuffix(tesString, akhiran[i])
-       if tesString is not rootword:
-         for j in range(len(awalan2)):
-            rootword=delPrefix(rootword, awalan2[j])
-         break
-    print(rootword)
-    # tahap 5 algoritma Tala     
-    tesString = "bersamaan"       
-    for i in range(len(awalan2)):
-       rootword=delPrefix(tesString, awalan2[i])
-       if tesString is not rootword:
-        # print(rootword)
-        break
-    tesString = rootword
-    for i in range(len(akhiran)):
-       rootword=delSuffix(rootword, akhiran[i])
-       if tesString is not rootword:
-        print(rootword)
-        break
+        tesString = j
+        for i in range(len(partikel)):
+            rootword=delSuffix(tesString, partikel[i])
+            if tesString is not rootword:
+                break
+        tesString=rootword
+        
+        # tahap 2 algoritma Tala     
+        for i in range(len(kepunyaan)):
+            rootword=delSuffix(tesString, kepunyaan[i])
+            if tesString is not rootword:
+                break
+        tesString=rootword
+        # tahap 3 algoritma Tala      
+        for c in range(len(awalan1)):
+            rootword=delPrefix(tesString, awalan1[c])
+            print(c)
+            if tesString is not rootword:
+                tesString=rootword
+                # tahap 4 algoritma Tala 
+                for i in range(len(akhiran)):  
+                    rootword=delSuffix(tesString, akhiran[i])
+                    if tesString is not rootword:
+                        for j in range(len(awalan2)):
+                            rootword=delPrefix(rootword, awalan2[j])
+                        break
+                tesString=rootword
+                break
+            else:
+                tesString=rootword
+                # tahap 5 algoritma Tala        
+                for i in range(len(awalan2)):
+                    rootword=delPrefix(tesString, awalan2[i])
+                    if tesString is not rootword:
+                        # print(rootword)
+                        break
+                tesString = rootword
+                for i in range(len(akhiran)):
+                    rootword=delSuffix(rootword, akhiran[i])
+                    if tesString is not rootword:
+                        break
+                tesString=rootword
+                break
+        stem.append(tesString)
     
+   
     #buat column
     dic = dict()
     dic.update({'Token':d})
@@ -136,10 +138,13 @@ def main():
     for idx, val in enumerate(d_index):
         # s = 'D' + str(idx+1)
         s = 'Frekuensi kata' 
+        z = 'Hasil Stemming'
         header.append(s)
+        header.append(z)
+        
     for idx, val in enumerate(d_index):
         dic.update({header[idx+1]:d_index[idx]})
-
+    dic.update({'Hasil Stemming':stem})
     
     
     # #hitung tf
@@ -189,8 +194,8 @@ def main():
     #export
     data = pd.DataFrame.from_dict(dic)
     data = data.round(decimals=3)
-    data.to_excel('test.xlsx', sheet_name="sheet1", index=False)
-    print("ekstrak token berhasil, silahkan buka file test.xlsx")
+    data.to_excel('StemmingTala.xlsx', sheet_name="sheet1", index=False)
+    print("ekstrak token berhasil, silahkan buka file StemmingTala.xlsx")
     
 
 main()
